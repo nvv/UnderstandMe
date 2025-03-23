f59725a68472bc058c067a9740639cd1195b45cc
vlad <vladislavn@pressreader.com>
2025-03-22 21:53:47 -0700

    translation screen adjustments

--- translation-screen/src/main/kotlin/com/vnamashko/undertsndme/translation/screen/TranslationScreen.kt
+++ translation-screen/src/main/kotlin/com/vnamashko/undertsndme/translation/screen/TranslationScreen.kt
@@ -1,14 +1,21 @@
 package com.vnamashko.undertsndme.translation.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,14 +27,13 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.vnamashko.undertsndme.language.picker.LanguageSelectionControl
 import com.vnamashko.undertsndme.language.picker.LanguageFor
 import kotlinx.coroutines.flow.debounce
 import com.vnamashko.understandme.translation.model.Language
+import kotlinx.coroutines.FlowPreview
 
 @Composable
 fun TranslationScreen(
@@ -49,16 +55,17 @@ fun TranslationScreen(
     }
 }
 
+@OptIn(FlowPreview::class)
 @Composable
 fun TranslationInputOutput(
     onTextChanged: (String) -> Unit,
     translation: String?,
     modifier: Modifier = Modifier
 ) {
-    var value by remember { mutableStateOf("") }
+    var textToTranslate by remember { mutableStateOf("") }
 
-    LaunchedEffect(value) {
-        snapshotFlow { value }
+    LaunchedEffect(textToTranslate) {
+        snapshotFlow { textToTranslate }
             .debounce(250)
             .collect { text ->
                 onTextChanged(text)
@@ -66,35 +73,55 @@ fun TranslationInputOutput(
     }
 
     Column(modifier = modifier.padding(8.dp)) {
+        Row(modifier = Modifier.height(24.dp)) {
+            Spacer(Modifier.weight(1f))
+
+            if (translation != null) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Clear",
+                    modifier = Modifier.clickable {
+                        textToTranslate = ""
+                        onTextChanged("")
+                    }
+                )
+            }
+        }
+
         OutlinedTextField(
-            value = value,
+            value = textToTranslate,
             placeholder = {
                 Text(
                     "Enter text",
-                    style = TextStyle(color = MaterialTheme.colorScheme.secondary),
-                    fontSize = 18.sp
+                    style = MaterialTheme.typography.titleLarge
                 )
             },
-            onValueChange = { value = it },
-            colors = androidx.compose.material3.OutlinedTextFieldDefaults.colors(
+            onValueChange = { textToTranslate = it },
+            colors = OutlinedTextFieldDefaults.colors(
                 unfocusedBorderColor = Color.Transparent,
                 focusedBorderColor = Color.Transparent,
             ),
+            textStyle = MaterialTheme.typography.titleLarge,
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(bottom = 8.dp)
         )
 
         if (translation != null) {
             Column {
                 Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
                     HorizontalDivider(
-                        color = MaterialTheme.colorScheme.tertiary,
-                        thickness = 1.dp,
-                        modifier = Modifier.fillMaxWidth(0.6f)
+                        color = MaterialTheme.colorScheme.primary,
+                        thickness = 1.5.dp,
+                        modifier = Modifier.fillMaxWidth(0.5f)
                     )
                 }
 
-                Text(text = translation)
+                Text(
+                    text = translation,
+                    style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.primary),
+                    modifier = modifier.padding(16.dp)
+                )
             }
         }
 
